f9dbc5364127080886ea69276474554542089b3f
barry <qiuxiaobing90@126.com>
2016-05-30 09:33:53 +0800

    commit

--- Phone.go
+++ Phone.go
@@ -23,7 +23,7 @@ type Phone struct {
 	Overheat_count int
 }
 
-func (phone Phone) append_conn(conn net.TCPConn, address net.TCPAddr) error {
+func (phone Phone) append_conn(conn net.TCPConn) error {
 	phone.mu.Lock()
 
 	err := conn.SetKeepAlive(true)
@@ -37,6 +37,8 @@ func (phone Phone) append_conn(conn net.TCPConn, address net.TCPAddr) error {
 	}
 
 	phone.Conn_list = append(phone.Conn_list, conn)
+
+	address := conn.RemoteAddr()
 
 	if len(phone.Conn_list) > phone.Overhead {
 		conn0 := phone.Conn_list[0]
@@ -172,12 +174,13 @@ func read_phones_from_file() {
 		if err != nil || io.EOF == err {
 			break
 		}
-		infos := strings.Split(" ", line)
+		log.Println(line)
+		infos := strings.Split(line, " ")
 		if len(infos) == 2 {
 			phone := Phone{}
 			phone.User_name = infos[0]
 			phone.Random = infos[1]
-			phones = append(phones, phone)
+			phones[infos[0]] = phone
 		}
 	}
 }
@@ -189,7 +192,7 @@ func start_phones() {
 
 	read_phones_from_file()
 
-	add, err := net.ResolveTCPAddr("tcp", ":8110")
+	add, err := net.ResolveTCPAddr("tcp", ":110")
 	if err != nil {
 		log.Println("error listen:", err)
 		return
@@ -199,6 +202,7 @@ func start_phones() {
 		log.Println("error listen:", err)
 		return
 	}
+	fmt.Println("start listen 110")
 	defer listen.Close()
 	log.Println("listen ok")
 
@@ -247,26 +251,24 @@ func process_phone_conn(conn net.TCPConn) {
 		log.Println("conn read error:", err)
 		return
 	}
-	first_line := string(buf[:n])
+	content := string(buf[:n])
 
-	fmt.Println(first_line)
-
-	pos := strings.Index(first_line, "/")
+	pos := strings.Index(content, "/")
 	if pos == -1 {
 		conn.Close()
 		return
 	}
 
-	if strings.HasPrefix(first_line, "GET /register_") {
-		req, err := getRequestInfo(first_line)
+	if strings.HasPrefix(content, "GET /register_") {
+		req, err := getRequestInfo(content)
 		if err != nil {
 			log.Println("conn read error:", err)
 			return
 		}
 		infos := strings.Split(req.RequestURI, "/")
-		user_name := infos[1]
-		random := infos[2]
-		version := infos[3]
+		user_name := infos[2]
+		random := infos[3]
+		version := infos[4]
 		log.Println("user_name:" + user_name + ";random:" + random + ";version:" + version)
 
 		if len(user_name) == 0 {
@@ -275,25 +277,59 @@ func process_phone_conn(conn net.TCPConn) {
 			return
 		}
 
-		for _, one_phone :=range phones {
-			if (one_phone.User_name == user_name &&	one_phone.Random != random ){
-				conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nUsername is already used."))
-				conn.Close()
-				return
-			}
+		if _, ok := phones[user_name]; ok && phones[user_name].Random != random {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nUsername is already exists."))
+			conn.Close()
+			return
 		}
 
 		phone := Phone{}
 		phone.User_name = user_name
 		phone.Random = random
 		phone.add_to_file()
+		phones[user_name] = phone
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nOK"))
 		conn.Close()
 		return
-
 	}
 
-	if strings.HasPrefix(first_line, "WEBKEY") {
+	if strings.HasPrefix(content, "WEBKEY") {
+		// WEBKEY username/random/version:port
+		lines := strings.Split(content, "/r/n")
+		if len(lines) > 0 {
+			first_line := lines[0]
+			p1 := strings.Index(first_line, "/")
+			user_name := first_line[7:p1]
+			p2 := strings.Index(first_line[p1 + 1:], "/") + p1 + 1
+			random := first_line[p1 + 1 : p2]
+			p3 := strings.Index(first_line[p2 + 1:], "/") + p2 + 1
+			version := first_line[p2 + 1 : p3]
+			port := first_line[p3 + 1:]
+			fmt.Println(user_name, random, version, port)
+
+			if len(user_name) <= 0 || len(random) <= 0 {
+				conn.Write([]byte("stop"))
+				conn.Close()
+				return
+			}
+
+			if _, ok := phones[user_name]; ok && phones[user_name].Random == random {
+				phones[user_name].append_conn(conn)
+				return
+			} else if _, ok := phones[user_name]; !ok {
+				phone := Phone{}
+				phone.User_name = user_name
+				phone.Random = random
+				phone.add_to_file()
+				phones[user_name] = phone
+				phones[user_name].append_conn(conn)
+			} else {
+				conn.Write([]byte("stop"))
+				conn.Close()
+			}
+			return
+		}
+
 	}
 
 }
--- forward.go
+++ forward.go
@@ -5,19 +5,22 @@ import (
 	"strings"
 	"net/http"
 	"log"
+	"fmt"
 )
 
-var phones [] Phone
+var phones map[string]Phone
 var db_file_name string = "phones.txt"
 
 func main() {
-
+	phones = make(map[string]Phone)
+	test()
+	start_phones()
 	http.HandleFunc("/", processClientReq) //设置访问的路由
 	err := http.ListenAndServe(":8000", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-	start_phones()
+	fmt.Println("start listen 110")
 }
 
 func getRequestInfo(str string) (http.Request, error) {
@@ -30,3 +33,6 @@ func getRequestInfo(str string) (http.Request, error) {
 	return *req, nil
 }
 
+func test() {
+	return
+}
